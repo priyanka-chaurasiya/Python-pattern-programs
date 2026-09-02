f5a242d070b53bc48a3baba092989dbb03bd083e
Priyanka Chaurasiya <priyankachaurasiya6509@gmail.com>
2026-09-02 22:55:33 +0530

    Add files via upload

---
+++ Right-Aligned Decreasing +. Increasing.py
@@ -0,0 +1,13 @@
+n = 7
+
+for i in range(n, 0, -1):
+    print("  " * (n - i), end="")
+    for j in range(i):
+        print("*", end=" ")
+    print()
+
+for i in range(2, n + 1):
+    print("  " * (n - i), end="")
+    for j in range(i):
+        print("*", end=" ")
+    print()
